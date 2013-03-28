037ee5e5d560da59fda8af534907ed37a7cb5373
Wayne <wayngray@gmail.com>
2013-03-28 16:06:47 +0200

    HomeModel update. Added logging.

--- Models/HomeModel.php
+++ Models/HomeModel.php
@@ -11,18 +11,25 @@ class Model {
     }
 
     public function getlogin() {
+
         if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
-            $query = "SELECT TOP(1) UserID FROM M_Users  WHERE UserName = '" . $_REQUEST['username'] . "' AND Password='" . $_REQUEST['password'] . "'";
+            $username = $_REQUEST['username'];
+            $password = $_REQUEST['password'];
+            $query = "SELECT TOP(1) UserID FROM M_Users  WHERE UserName = '" . $username . "' AND Password='" . $password . "'";
             $result = $this->dbhelper->Select($query);
-            if ($result[0] === "") {
-                return false;
-            } else {
+
+            if (is_array($result)) {
+                $query = " INSERT INTO L_Login(UserNameSubmitted, PasswordSubmitted, Success, UserIDReturned)
+                            VALUES('" . $username . "','" . $password . "',1,'" . $result[0] . "')";
+                $this->dbhelper->LogLogin($query);
                 return $result[0];
+            } else {
+                $query = " INSERT INTO L_Login (UserNameSubmitted, PasswordSubmitted, Success, UserIDReturned)
+                            VALUES('" . $username . "','" . $password . "',0,'')";
+                print $query;
+                $this->dbhelper->LogLogin($query);
+                return "false";
             }
-            echo $result;
-            return $result;
-        } else {
-            echo 'First Time ';
         }
     }
 
