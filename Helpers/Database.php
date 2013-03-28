872bb4f8669babff4b8dc7bed0df605f12557147
Wayne <wayngray@gmail.com>
2013-03-28 16:07:02 +0200

    Database update. Added logging.

--- Helpers/Database.php
+++ Helpers/Database.php
@@ -18,19 +18,14 @@ class Database {
 
    public function LogLogin($query) {
         $conn = $this->ConnectToDb();
-        //$tsql = "SELECT     TOP (1) UserID FROM M_Users";
         $stmt = sqlsrv_query($conn, $query);
         if ($stmt === false) {
             echo "Error in executing query.</br>";
             die(print_r(sqlsrv_errors(), true));
         }
-        //echo "User logindadsadas: " . $result[0] . "</br>";
-        $result = sqlsrv_fetch_array($stmt);
         sqlsrv_free_stmt($stmt);
         $this->CloseDbConnection($conn);
-        return $result;
-    } 
-    /* Specify the server and connection string attributes. */
+    }
 
     private function ConnectToDb() {
         $serverName = "(local)";
